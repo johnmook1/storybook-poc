fa0afa64ee53103f50026db8b824cbc552c60106
John Mook <john.mook1@adusa.com>
2025-11-14 16:17:09 -0600

    Adds storybook/vue3-vite

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+};
+export default config;
